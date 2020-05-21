dc547205f02450ea8ab40df0a25b56ac5d389bf2
whatsyourask <lexeymartynov@gmail.com>
2020-05-21 03:39:27 +0300

    remove while in thread func

--- cppAndCsharp/OS/os_7.cpp
+++ cppAndCsharp/OS/os_7.cpp
@@ -4,8 +4,6 @@
 #include <strsafe.h>
 using namespace std;
 
-#define BUF_SIZE 255
-
 // Функция для всех философов
 void philosophers();
 // Функция активности философов
@@ -41,8 +39,7 @@ void philosophers() {
 		NULL
 		);
 	// цикл по каждому философу
-	int handle = 0;
-	while (handle < ph_count) {
+	for (int handle = 0; handle < ph_count; handle++) {
 		// Создать i-ый мьютекс,
 		// не позволяя наследовать процесс, 
 		// не позволяя процессу владеть мьютексом, 
@@ -58,13 +55,12 @@ void philosophers() {
 			0,
 			0
 			);
-		handle++;
 	}
 	// Ожидать завершения работы всех созданных потоков
 	// или пока не истечёт интервал(в данном случае бесконечность)
-	WaitForMultipleObjects(handle, hThreadArray, TRUE, INFINITE);
+	WaitForMultipleObjects(ph_count, hThreadArray, TRUE, INFINITE);
 	// Закрыть дескрипторы потоков и мьютексов
-	for (int i = 0; i < handle; i++) {
+	for (int i = 0; i < ph_count; i++) {
 		CloseHandle(hThreadArray[i]);
 		CloseHandle(hMutexArray[i]);
 	}
@@ -80,53 +76,42 @@ DWORD WINAPI activity(LPVOID lp_param) {
 	// Философ берёт правую палочку у соседа справа,
 	// для последнего философа в массиве, сосед справа это 0 философ
 	int right_stick;
-	right_stick	= (philosoph_number != ph_count - 1) ? philosoph_number + 1 : 0;
+	right_stick = (philosoph_number != ph_count - 1) ? philosoph_number + 1 : 0;
 	srand(GetTickCount64() + philosoph_number);
 	// цикл по кол-ву необходимых обедов для 1 философа
 	for (int dinner = 0; dinner < iterations; dinner++) {
-		// Кол-во символов, записанных в строку
-		int write_char_count = 0;
 		// Размышление философа
-		int think_time = (rand() % 5 + 1);
+		int think_time = (rand() % 5 + 1)*1000;
 		// Печать сообщения, что философ размышляет
 		printf("%d's thinking\n", philosoph_number);
 		Sleep(think_time);
-		bool try_to_have_lunch = true;
-		DWORD wait_result;
 		// Обед
-		while (try_to_have_lunch) {
-			// Попытаться войти в семафор(взять стул, например)
-			wait_result = WaitForSingleObject(semaphore, INFINITE);
-			// Если вошёл(сел за стол)
-			if (wait_result == WAIT_OBJECT_0) {
-				// Поток ожидает получение мьютекса левой палочки
-				WaitForSingleObject(hMutexArray[left_stick], INFINITE);
-				// Печать сообщения, что философ взял левую палочку
-				printf("%d took left stick\n", philosoph_number);
-				// Поток ожидает получение мьютекса правой палочки
-				WaitForSingleObject(hMutexArray[right_stick], INFINITE);
-				// Печать сообщения, что философ взял правую палочку
-				printf("%d took right stick\n", philosoph_number);
-				// Начать обедать, если обе палочки свободны
-				try_to_have_lunch = false;
-				int lunch_time = (rand() % 3 + 1);
-				// Печать сообщения, что философ ест
-				printf("%d's eating\n", philosoph_number);
-				Sleep(lunch_time);
-				// Освобождение потока от мьютекса левой палочки
-				ReleaseMutex(hMutexArray[left_stick]);
-				// Освобождение потока от мьютекса правой палочки
-				ReleaseMutex(hMutexArray[right_stick]);
-				ReleaseSemaphore(
-					semaphore,	  // дескриптор семафора
-					1,            // увеличить счетчик семафора на 1
-					NULL		  // Предыдущее значение не нужно
-					);
-			}
-			// Если не вошёл в семафор
-			// Ждать пока освободится семафор(стул)
-			// т.е. переход к следующей итерации
-		}
+		// Попытаться войти в семафор(взять стул, например)
+		// Если не может войти, ждёт
+		WaitForSingleObject(semaphore, INFINITE);
+		// Если вошёл(сел за стол)
+		// Поток ожидает получение мьютекса левой палочки
+		WaitForSingleObject(hMutexArray[left_stick], INFINITE);
+		// Печать сообщения, что философ взял левую палочку
+		printf("%d took left stick\n", philosoph_number);
+		// Поток ожидает получение мьютекса правой палочки
+		WaitForSingleObject(hMutexArray[right_stick], INFINITE);
+		// Печать сообщения, что философ взял правую палочку
+		printf("%d took right stick\n", philosoph_number);
+		// Начать обедать, если обе палочки свободны
+		int lunch_time = (rand() % 3 + 1)*1000;
+		// Печать сообщения, что философ ест
+		printf("%d's eating\n", philosoph_number);
+		Sleep(lunch_time);
+		// Освобождение потока от мьютекса левой палочки
+		ReleaseMutex(hMutexArray[left_stick]);
+		// Освобождение потока от мьютекса правой палочки
+		ReleaseMutex(hMutexArray[right_stick]);
+		ReleaseSemaphore(
+			semaphore,	  // дескриптор семафора
+			1,            // увеличить счетчик семафора на 1
+			NULL		  // Предыдущее значение не нужно
+			);
 	}
 	return 0;
 }
