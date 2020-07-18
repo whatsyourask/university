594e7bbe3334366c73a13b39c80ec9d417bd59c1
whatsyourask <lexeymartynov@gmail.com>
2020-07-18 16:42:44 +0300

    managing python programs and deleting Ruby folder

--- Rubic/graph/test.rb
+++
@@ -1,5 +0,0 @@
-load 'graph.rb'
-
-gr = Graph.new(6)
-gr.write_matrix
-gr.search_way(2,6)
--- Rubic/mixins/main.rb
+++
@@ -1,7 +0,0 @@
-load 'main_classes.rb'
-
-# p1 = Point.new(15, 30)
-# p2 = Point.new(20, 30)
-# puts p1.distance(p2)
-
-
