0872c7016a6c39addd580ac40db2b2fe20e58702
whatsyourask <lexeymartynov@gmail.com>
2020-09-08 17:33:01 +0300

    Add a method to find invert and a method to compute the devision of 2 elements

--- toci_lab1.py
+++ toci_lab1.py
@@ -12,31 +12,54 @@ class Ring:
         self._num = num
 
     def __add__(self, other):
+        # + operator overload
         return Ring(self._n, (self._num + other.num) % self._n)
 
     def __sub__(self, other):
+        # - operator overload
         if other.num < 0:
             other.num += self._n
         return Ring(self._n, (self._num - other.num) % self._n)
 
     def __mul__(self, other):
+        # * operator overload
         return Ring(self._n, (self._num * other.num) % self._n)
 
     def _lines_count(self):
+        # Compute lines count
         a = self._n
         b = self._num
-        lines_count = 1
+        lines_count = 0
+        # To save divs or a/b
+        self._divs = []
         while True:
             mod = a % b
+            div = a // b
+            self._divs.append(div)
             if not mod:
                 break
             a = b
             b = mod
             lines_count += 1
+        self._divs.pop()
         return lines_count
 
     def invert(self):
-        self._lines_count()
+        # Find a invert of current num
+        count = self._lines_count()
+        x = 0
+        y = 1
+        while self._divs:
+            temp = x
+            x = y
+            last = self._divs.pop()
+            y = temp - y * last
+        return Ring(self._n, y)
+
+    def __truediv__(self, other):
+        # / operator overload
+        invert_other = other.invert()
+        return self.__mul__(invert_other)
 
 
 def some_code():
@@ -48,7 +71,10 @@ def some_code():
     print(f'{a.num} - {b.num} = {c.num}')
     c = a * b
     print(f'{a.num} * {b.num} = {c.num}')
-    b.invert()
+    c = b.invert()
+    print(f'invert of {b.num} = {c.num}')
+    c = a/b
+    print(f'{a.num} / {b.num} = {c.num}')
 
 
 some_code()
