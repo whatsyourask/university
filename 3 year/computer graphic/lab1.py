b6b1bc3587669dc47e8ebd43691acc22d183a26f
whatsyourask <lexeymartynov@gmail.com>
2020-09-14 18:02:23 +0300

    Complete a current task

--- 3 year/computer graphic/lab1.py
+++ 3 year/computer graphic/lab1.py
@@ -1,17 +1,19 @@
 import numpy as np
+from typing import Tuple, List
 
 
-def get_file_data(filename):
+def get_file_data(filename) -> str:
     f    = open(filename, 'r')
     data = f.read()
     return data
 
 
-def get_max_min(points):
+def get_max_min(points) -> Tuple:
     x = []
     y = []
     z = []
     for point in points:
+        # Put all x in the list of x and so on with y and z
         x.append(point[1])
         y.append(point[2])
         z.append(point[3])
@@ -24,7 +26,8 @@ def get_max_min(points):
     return max_x, min_x, max_y, min_y, max_z, min_z
 
 
-def find_start_f(data):
+def find_start_f(data) -> int:
+    # Count lines before first f
     line_number = 0
     for line in data:
         if 'f' in line:
@@ -34,28 +37,55 @@ def find_start_f(data):
     return line_number
 
 
-def surface_area(point1, point2, point3):
-    pass
+def surface_area(points) -> int:
+    # Create matrices with ones
+    matrix1 = np.ones((3, 3))
+    matrix2 = np.ones((3, 3))
+    matrix3 = np.ones((3, 3))
+    # Cast list to np array and reshape it in matrix
+    points = np.array(points).reshape(3, 3)
+    # Copy by columns
+    matrix1[:,1] = points[:,0]
+    matrix1[:,2] = points[:,1]
+
+    matrix2[:,1] = points[:,0]
+    matrix2[:,2] = points[:,2]
+
+    matrix3[:,1] = points[:,1]
+    matrix3[:,2] = points[:,2]
+
+    half = 0.5
+    # Find determinants of matrices
+    determ1 = np.linalg.det(matrix1)
+    determ2 = np.linalg.det(matrix2)
+    determ3 = np.linalg.det(matrix3)
+    area = half * np.sqrt(determ1 ** 2 + determ2 ** 2 + determ3 ** 2)
+    return area
 
 
-def surface_area_all(points, edges):
+def surface_area_all(points, edges) -> int:
     all = 0
     for edge in edges:
         ind1 = int(edge[1]) - 1
         ind2 = int(edge[2]) - 1
         ind3 = int(edge[3]) - 1
-        all += surface_area(points[ind1], points[ind2], points[ind3])
+        all += surface_area([points[ind1][1:],
+                             points[ind2][1:],
+                             points[ind3][1:]])
     return all
 
 
 def main():
     filename = 'teapot.obj'
     data     = get_file_data(filename).split('\n')
+    # Split data and cast it to list
     data     = [elem.split(' ') for elem in data]
+    # Remova emptry strings
     data.remove([''])
     data.remove([''])
     data    = np.array(data)
     start_f = find_start_f(data)
+    # Separate data like points and edges
     points  = data[:start_f]
     edges   = data[start_f:]
     points_count, edges_count = len(points), len(edges)
@@ -68,8 +98,9 @@ def main():
     print(f'min y = {max_min[3]}')
     print(f'max z = {max_min[4]}')
     print(f'min z = {max_min[5]}')
-    surface_area_all(points, edges)
+    area = surface_area_all(points, edges)
+    print(f'teapot area = {area}')
 
 
 if __name__=='__main__':
-  main()
+    main()
