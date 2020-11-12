b1bcf00b3e12e82b534dc5835e938f028f03139b
whatsyourask <lexeymartynov@gmail.com>
2020-11-12 19:28:36 +0300

    Add comments

--- 3 year/botcb/lab2/app.py
+++ 3 year/botcb/lab2/app.py
@@ -4,15 +4,23 @@ from time import time
 
 class Application:
     def run(self) -> None:
+        # Создать все элементы
         self._create_all_elements()
+        # Создать объект класса RSA
         self._rsa = RSA()
+        # Запустить приложение с интерфейсом
         self._gui.start()
 
     def _create_all_elements(self) -> None:
+        # Создание объекта интерфейса из лабораторной 1
         self._gui = GraphicUI()
+        # Назначение заголовка приложения
         self._gui.set_title('Lab №2')
+        # Ширина ячеек в таблице окна
         self._width = 30
+        # Создание метки с текстом
         self._gui.label('p ', 1, 1, E)
+        # Создание поля для переменной
         self._p_field, _ = self._gui.entry(IntVar(), 2, 1, self._width, W)
         self._gui.label('q ', 1, 2, E)
         self._q_field, _ = self._gui.entry(IntVar(), 2, 2, self._width, W)
@@ -38,21 +46,33 @@ class Application:
         self._gui.label('Время дешифрования ', 1, 11, E)
         self._d_t_field, _ = self._gui.entry(IntVar(), 2, 11,
                                              self._width, W)
+        # Создание кнопки с передачей ей функции
+        # которую она будет выполнять при нажатии
         self._gui.button('Генерация ключей шифрования', 2, 12,
                          self._width, W, self._generate_keys_event)
+        # Переменная для сохранения элемента интерфейса с предупреждением
+        # о величине m
         self._m_warning = None
         self._gui.button('Зашифровать сообщение', 2, 13,
                          self._width, W, self._encrypt_message_event)
+        # Переменная для сохранения элемента интерфейса с предупреждением
+        # о величине c
         self._c_warning = None
         self._gui.button('Дешифровать сообщение', 2, 14,
                          self._width, W, self._decrypt_message_event)
 
     def _generate_keys_event(self) -> None:
+        # Функция для кнопки генерации ключей
+        # Достаем p и q из полей
         p = self._p_field.get()
         q = self._q_field.get()
+        # Проверяем, если не нули
         if p and q:
+            # Создаём объект со своими параметрами
             self._rsa = RSA(p, q)
+            # Генерируем ключи
             self._rsa.generate_public_key()
+            # Присваиваем все поля класса нужным полям интерфейса
             self._n_field.set(self._rsa.n)
             self._euler_f_field.set(self._rsa.euler_func)
             self._e_field.set(self._rsa.e)
@@ -60,27 +80,43 @@ class Application:
             self._d_field.set(self._rsa.d)
 
     def _encrypt_message_event(self) -> None:
+        # Для кнопки шифрования сообщения
+        # Достаем из полей m и n
         m = self._m_field.get()
         n = self._n_field.get()
+        # Если m >= n то вывести предупреждение
         if m >= n:
             self._m_warning = self._gui.label('! m < n', 3, 7, W)
             return
+        # Если же нет, то продолжить шифрование
+        # Если до этого выводили предупреждения, то надо их убрать
+        # т.к m прошло проверку
         if self._m_warning:
+            # Удалить с фрейма
             self._m_warning.grid_remove()
         if self._c_warning:
             self._c_warning.grid_remove()
+        # Получить текущее время
         start = time()
+        # Взять ключ из поля
         e = self._e_field.get()
+        # Проверяем не равны ли нулю e и n
         if n != self._rsa.n:
             self._rsa.n = n
         if e != self._rsa.e:
             self._rsa.e = e
+        # Шифрование в объекте класса RSA
         c = self._rsa.encrypt(m)
         self._c_field.set(c)
+        # Смотрим время после шифрования
         end = time()
+        # Вычисляем время шифрования
         self._e_t_field.set(end - start)
 
     def _decrypt_message_event(self) -> None:
+        # Полностью аналогичный метод, только разные переменные
+        # Написать всё в один метод нельзя, т.к нельзя передать параметры метода
+        # кнопке напрямую, мы можем взять значения только внутри функции
         c = self._c_field.get()
         n = self._n_field.get()
         if c >= n:
