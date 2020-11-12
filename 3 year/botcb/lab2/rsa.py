23a2b4939f038bcba53929cab91ef440d6d60e8f
whatsyourask <lexeymartynov@gmail.com>
2020-11-12 19:18:45 +0300

    Add comments

--- 3 year/botcb/lab2/rsa.py
+++ 3 year/botcb/lab2/rsa.py
@@ -1,4 +1,6 @@
 import sys
+# sys.path хранит стандартные пути, где python ищет модули для импорта
+# Теперь мы можем импортить модули вне папки 2 лабораторной
 sys.path.insert(0, '..')
 import lab1
 from lab1.euler import gcd
@@ -9,7 +11,11 @@ from typing import List
 
 
 class RSA:
+    """Класс для генерации ключей RSA, шифрования и дешифрования сообщений
     def __init__(self, p=None, q=None) -> None:
+        # Конструктор с параметрами по умолчанию
+        # для того, чтобы можно было создать класс без всего
+        # и заполнять поля по мере надобности
         self._p = p
         self._q = q
         self._e = None
@@ -17,15 +23,24 @@ class RSA:
         self._d = None
 
     def generate_public_key(self) -> None:
+        # Генерация публичного ключа
+        # Вычисление n = p * q
         self._n = self._p * self._q
+        # Вычисление функции эйлера = (p - 1) * (q - 1)
         self._euler_func = (self._p - 1) * (self._q - 1)
+        # Массив для сохранения всех возможных публичных ключей
         e_arr = []
         for e in range(self._n + 1):
+            # Перебираем числа и проверяем взаимно простые ли они с e_f(n)
             if gcd(e, self._euler_func) == 1:
                 e_arr.append(e)
+        # Генерируем случайный индекс
         ind = randrange(len(e_arr))
+        # Присваиваем из возможных ключ с этим индексом
         self._e = e_arr[ind]
 
+    # Геттеры и сеттеры для полей, т.к. они с _
+    # Доступны только классам, которые наследуются от RSA
     @property
     def n(self) -> int:
         return self._n
@@ -59,7 +74,10 @@ class RSA:
         self._e = e
 
     def generate_private_key(self) -> None:
+        # Генерация приватного ключа
+        # Использую класс Ring из 1 лабораторной
         e = Ring(self._euler_func, self._e)
+        # Нахожу обратное от е
         invert_e, _ = e.invert()
         self._d = invert_e.num
 
@@ -72,12 +90,16 @@ class RSA:
         self._d = d
 
     def encrypt(self, m) -> int:
+        # Шифровать только, если были генерация ключа или присвоение поля e
         encrypted = None
         if self._e:
+            # Использую функцию из 1 лаборатнорной для быстрого возведения
+            # в степень по модулю
             encrypted, _ = fast_powers_mod(self._n, m, self._e)
         return encrypted
 
     def decrypt(self, c) -> int:
+        # Шифровать только, если были генерация ключа или присвоение поля d
         decrypted = None
         if self._d:
             decrypted, _ = fast_powers_mod(self._n, c, self._d)
@@ -85,6 +107,7 @@ class RSA:
 
 
 def test():
+    # Функция для проверки
     rsa = RSA(17, 31)
     rsa.generate_public_key()
     print(f'Chosen e = {rsa.e}')
@@ -98,7 +121,6 @@ def test():
     print(f'decrypted message {c} => {new_m}')
     if m == new_m:
         print('Test passed')
-
 
 
 if __name__=='__main__':
