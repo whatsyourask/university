2f060e1967a03f8b12a5dde1230f5e9a110e83dc
whatsyourask <lexeymartynov@gmail.com>
2020-12-29 16:40:17 +0900

    Complete the lab

--- 3 year/botcb/lab4/MillerRabin.py
+++ 3 year/botcb/lab4/MillerRabin.py
@@ -7,65 +7,89 @@ import random
 from lab1.fast_powers_mod import fast_powers_mod
 import math
 from lab1.euler import gcd
+from typing import Tuple
 
 
-def miller_rabin_test(n: int, a: int = None) -> bool:
+def miller_rabin_test(n: int, a: int = None) -> Tuple:
+    # Метод для вычисления теста Миллера-Рабина
     temp = n - 1
     s    = 0
+    # Делим n - 1 на 2, пока не будет остатка больше нуля
     while not temp % 2:
         s    += 1
         temp /= 2
     t = int(temp)
+    # Если параметр a не передан, рандомно берём из диапазона
     if not a:
         a = random.randint(2, n - 2)
+    # Смотрим не выходит ли a за диапазон
+    elif a >= n - 1:
+        a -= 1
+    elif a >= n - 2:
+        a -= 2
+    # Проверяем 1 условие
     mod_result, _ = fast_powers_mod(n, a, t)
-    if mod_result and mod_result == n - 1:
-        return True
+    if mod_result == 1 or mod_result == n - 1:
+        return True, (a, t)
+    # Проверяем 2 условие
     if not s:
         s += 1
-    for i in range(s - 1):
-        mod_result = fast_powers_mod(n, mod_result, 2)
+    # В цикле вычисляем быстрое возведение в степень по модулю и ищем равное n - 1
+    for i in range(1, s):
+        mod_result, _ = fast_powers_mod(n, mod_result, 2)
         if mod_result == n - 1:
-            return True
-    return False
+            return True, (a, t)
+    return False, (a, t)
 
 
-def research(begin: int, end: int, a: int):
+def research(begin: int, end: int, a: int) -> Tuple:
+    # По теореме n >= 5
     if begin < 5:
         begin = 5
+    # Нечётное
     if not begin % 2:
         begin += 1
+    # Если a больше begin, нет смысла начинать с begin
     if a > begin:
         begin = a
-    mistakes = 0
+    mistakes      = 0
     wrong_numbers = ''
     for n in range(begin, end + 1, 2):
-        miller_rabin_result = miller_rabin_test(n, a)
-        primary_result      = primary_test(n)
-        is_not_equal        = miller_rabin_test == primary_test
+        # Определяем число по тесту Миллера-Рабина и по обычному алгоритму
+        miller_rabin_result, _ = miller_rabin_test(n, a)
+        primary_result         = primary_test(n)
+        is_not_equal           = miller_rabin_result != primary_result
+        # Смотрим результаты и определяем ошибку
         if is_not_equal:
             mistakes += 1
-            wrong_numbers += str(n)
-        
+            wrong_numbers += str(n) + ' '
+    return mistakes, wrong_numbers
 
 
 def primary_test(n: int) -> bool:
-    end = math.sqrt(n)
-    for i in range(2, end):
-        if gcd(i, n) != 1:
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
 
 
 def test():
+    # тест модуля
     n      = 13
     answer = True
-    test_result = miller_rabin_test(n)
+    test_result, _ = miller_rabin_test(n)
     if test_result == answer:
-        print('[+] Test passed.')
+        print('[+] Miller Rabin test passed.')
     primary_result = primary_test(n)
     if primary_result == answer:
-        print('[+] Test passed.')
+        print('[+] Primary test passed.')
+    print(research(1, 100000, 5))
 
 
 if __name__=='__main__':
--- 3 year/botcb/lab4/app.py
+++ 3 year/botcb/lab4/app.py
@@ -15,4 +15,52 @@ class Application:
         # Создание объекта для графики из лабораторной 1
         self._gui   = GraphicUI()
         self._gui.set_title('Lab №3')
-        self._width = 12
+        self._width = 13
+        self._gui.label('n ',  1, 1, E)
+        self._n_field, _ = self._gui.entry(IntVar(), 2, 1, self._width, W)
+        self._gui.label('a ', 1, 2, E)
+        self._a_field, _ = self._gui.entry(IntVar(), 2, 2, self._width, W)
+        self._gui.label('t ', 1, 3, E)
+        self._t_field, _ = self._gui.entry(IntVar(), 2, 3, self._width, W)
+        self._gui.label('Результат ', 1, 4, E)
+        self._result_field, _ = self._gui.entry(StringVar(), 2, 4,
+                                                            self._width, W)
+        self._gui.button('Определить', 2, 5,
+                                self._width, W, self._define_number)
+        self._gui.label('Интервал ', 1, 6, E)
+        self._begin_field, _ = self._gui.entry(IntVar(), 2, 6, self._width, W)
+        self._end_field, _   = self._gui.entry(IntVar(), 3, 6, self._width, W)
+        self._gui.label('а в исследовании', 1, 7, E)
+        self._research_a_field, _ = self._gui.entry(IntVar(), 2, 7,
+                                                                self._width, W)
+        self._text = None
+        self._gui.button('Исследовать', 2, 8,
+                                self._width, W, self._research)
+
+    def _define_number(self) -> None:
+        # Метод для кнопки определения простоты
+        n = self._n_field.get()
+        result, (a, t) = miller_rabin_test(n)
+        self._a_field.set(a)
+        self._t_field.set(t)
+        result = 'Простое' if result else 'Составное'
+        self._result_field.set(result)
+
+    def _research(self) -> None:
+        # Метод для исследования числа ошибочных заключений теста
+        a     = self._research_a_field.get()
+        begin = self._begin_field.get()
+        end   = self._end_field.get()
+        start = time()
+        mistakes, wrong_numbers = research(begin, end, a)
+        end   = time()
+        calculation_time = end - start
+        explanation = 'Время вычисления: ' + str(calculation_time) + '\n'
+        explanation += 'Количество ошибок: ' + str(mistakes) + '\n'
+        explanation += 'Ошибки:\n' + wrong_numbers
+        text_width = (self._width + 4) * 3
+        text_height = 7
+        if self._text:
+            self._text.grid_remove()
+        self._text = self._gui.text(1, 9, text_width, text_height, W, 3)
+        self._text.insert(INSERT, explanation)
