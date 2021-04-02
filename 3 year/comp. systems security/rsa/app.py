618bf64a5850e5a5045e0f1361b34daccfc7eab4
whatsyourask <lexeymartynov@gmail.com>
2021-04-02 16:19:59 +0300

    It seems that RSA algorithm is completed

--- 3 year/comp. systems security/rsa/app.py
+++ 3 year/comp. systems security/rsa/app.py
@@ -2,7 +2,7 @@ import sys
 # To import FastModularExponentiation
 sys.path.insert(0, '..')
 from additive_cipher.gui import *
-from backend import miller_rabin_generate, euler_func
+from backend import miller_rabin_generate, RSA
 
 
 class Application:
@@ -30,27 +30,66 @@ class Application:
         self._n_field, _ = self._gui.entry(StringVar(), 2, 4, width, W)
         self._gui.label('Euler func', 1, 5, W)
         self._euler_func_field, _ = self._gui.entry(StringVar(), 2, 5, width, W)
+        self._gui.label('e', 1, 6, W)
+        self._e_field, _ = self._gui.entry(StringVar(), 2, 6, width, W)
+        self._gui.label('d', 1, 7, W)
+        self._d_field, _ = self._gui.entry(StringVar(), 2, 7, width, W)
+        self._gui.label('Cleartext', 1, 8, W)
+        self._cleartext_field = self._gui.text(2, 8, width, 5, W)
+        self._gui.label('Encrypted text', 1, 9, W)
+        self._encrypted_field = self._gui.text(2, 9, width, 5, W)
+        self._gui.button('Encrypt', 3, 9, width, W, self._encrypt)
+        self._gui.label('Decrypted text', 1, 10, W)
+        self._decrypted_field = self._gui.text(2, 10, width, 5, W)
+        self._gui.button('Decrypt', 3, 10, width, W, self._decrypt)
 
     def _generate(self) -> None:
+        # Method to generate necessary numbers
         try:
+            # Clear the fields
             self._p_field.set('')
             self._q_field.set('')
             self._n_field.set('')
             self._euler_func_field.set('')
+            self._e_field.set('')
+            self._d_field.set('')
+            # Get bits length
             bits_len = int(self._bits_field.get())
+            # If less than eigth then raise exception
             if bits_len < 8:
                 raise ValueError
+            # Generate p and q by Miller Rabit test
             p = miller_rabin_generate(bits_len)
             self._p_field.set(p)
             q = miller_rabin_generate(bits_len)
             self._q_field.set(q)
             n = p * q
             self._n_field.set(n)
-            euler_func_result = euler_func(n)
-            self._euler_func_field.set(euler_func_result)
+            euler_func = (p - 1) * (q - 1)
+            self._euler_func_field.set(euler_func)
+            # Generate public and private keys
+            self._rsa = RSA()
+            self._rsa.n = n
+            self._rsa.euler_func = euler_func
+            # Bits length needs to be more or equal than one third
+            e = self._rsa.generate_public_key(bits_len // 3)
+            self._e_field.set(e)
+            d = self._rsa.generate_private_key()
+            self._d_field.set(d)
         except ValueError:
             self._process_exception()
 
+    def _encrypt(self) -> None:
+        self._encrypted_field.delete("1.0", END)
+        cleartext = self._cleartext_field.get("1.0", END)[:-1]
+        encrypted = self._rsa.encrypt(cleartext)
+        self._encrypted_field.insert(INSERT, encrypted)
+
+    def _decrypt(self) -> None:
+        self._decrypted_field.delete("1.0", END)
+        encrypted = self._encrypted_field.get("1.0", END)[:-1]
+        decrypted = self._rsa.decrypt(encrypted)
+        self._decrypted_field.insert(INSERT, decrypted)
 
     def _process_exception(self) -> None:
         # Method to create a new window on top of main window
--- 3 year/comp. systems security/rsa/backend.py
+++ 3 year/comp. systems security/rsa/backend.py
@@ -4,25 +4,15 @@ sys.path.insert(0, '..')
 from miller_rabin_and_solovay_strassen.backend import MillerRabin
 from secrets import randbits
 from math import log2
+from extended_euclidean_algorithm_and_fast_modular_exponentiation.backend import ExtendedEuclideanAlgorithm
+from random import randint
 
 
 class RSA:
-    @property
-    def p(self) -> int:
-        return self._p
-
-    @p.setter
-    def p(self, p: int) -> None:
-        self._p = p
-
-    @property
-    def q(self) -> int:
-        return self._p
-
-    @q.setter
-    def q(self, q: int) -> None:
-        self._q = q
-
+    """
+    Class that generates public and private key, encrypts and decrypts any text
+    """
+    LAST_ORD = 8364
     @property
     def n(self) -> int:
         return self._n
@@ -31,24 +21,64 @@ class RSA:
     def n(self, n: int) -> None:
         self._n = n
 
+    @property
+    def euler_func(self) -> int:
+        return self._euler_func
+
+    @euler_func.setter
+    def euler_func(self, e_func: int) -> None:
+        self._euler_func = e_func
+
+    def generate_public_key(self, bits_len: int) -> int:
+        # To generate public key
+        e = randint(pow(2, bits_len), self._n)
+        while gcd(e, self._euler_func) != 1:
+            e += 1
+        self._e = e
+        return e
+
+    def generate_private_key(self) -> int:
+        # To generate private key
+        eea = ExtendedEuclideanAlgorithm()
+        eea.x = self._euler_func
+        eea.y = self._e
+        _, self._d, _ = eea.algorithm()
+        return self._d
+
+    def encrypt(self, cleartext: str) -> str:
+        encrypted = ' '.join(list(map(lambda char: self._chr_encrypt(char),
+                                      cleartext)))
+        return encrypted
+
+    def _chr_encrypt(self, char: str) -> str:
+        # To make lambda short
+        return str(pow(ord(char), self._e, self._n))
+
+    def decrypt(self, encrypted: str) -> str:
+        decrypted = ''.join(list(map(lambda seq: self._chr_decrypt(seq),
+                         encrypted.split())))
+        return decrypted
+
+    def _chr_decrypt(self, seq: str) -> str:
+        # To make lambda short
+        return chr(pow(int(seq), self._d, self._n) % self.LAST_ORD)
+
+
+def gcd(a, b) -> int:
+    while b:
+        a, b = b, a % b
+    return a
+
 
 def miller_rabin_generate(bits_len: int) -> int:
-    number = randbits(bits_len)
+    number = generate_one_time_properly(bits_len)
     while not MillerRabin.test(number, int(log2(number))):
-        number = randbits(bits_len)
+        number = generate_one_time_properly(bits_len)
     return number
 
 
-def euler_func(n: int) -> int:
-    # Euler function calculation
-    result = n
-    i = 2
-    while i ** 2 <= n:
-        if n % i == 0:
-            while n % i == 0:
-                n /= i
-            result -= result / i
-        i += 1
-    if n > 1:
-        result -= result / n
-    return result 
+def generate_one_time_properly(bits_len: int) -> int:
+    number = randbits(bits_len)
+    while number < 3:
+        number = randbits(bits_len)
+    return number
