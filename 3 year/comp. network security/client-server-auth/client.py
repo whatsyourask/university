71180097d3b2c2bb12dcdc852f781d74e9e0411c
whatsyourask <lexeymartynov@gmail.com>
2021-04-21 20:48:17 +0300

    Began an authentication

--- 3 year/comp. network security/client-server-auth/client.py
+++ 3 year/comp. network security/client-server-auth/client.py
@@ -1,4 +1,6 @@
 from socket import *
+from crypto import *
+import sys
 
 
 class AuthClient:
@@ -6,24 +8,45 @@ class AuthClient:
     Provides client functionality
     Like interface to work with socket and auth module
     """
-    def connect(self, addr: str, port: int) -> None:
+    def __init__(self, addr: str, port: int, bits_length: int) -> None:
+        self.__addr = addr
+        self.__port = port
+        self.__bits_length = bits_length
+
+    def connect(self) -> None:
         """Connect to a server"""
         # Create a socket object
         self.__socket = socket(AF_INET, SOCK_STREAM)
         # Check if it is a localhost
-        is_localhost = addr == ''
+        is_localhost = self.__addr == ''
         if is_localhost:
             # if it is then convert '' to localhost address
-            addr = '127.0.0.1'
-        print(f'Trying to connect to {addr}:{port}')
+            self.__addr = '127.0.0.1'
+        print(f'Trying to connect to {self.__addr}:{self.__port}')
         # Connect to the server
-        self.__socket.connect((addr, port))
-        print('Connection established')
+        self.__socket.connect((self.__addr, self.__port))
+        print('Connection established.')
+
+    def authenticate(self, login: str, password: str):
+        rsa, pub_key, priv_key = generate_keys(self.__bits_length)
+        self.__rsa = rsa
+        self.__pub_key = pub_key
+        self.__priv_key = priv_key
+        self.__socket.send(str(pub_key).encode('utf-8'))
+        server_pub_key = self.__socket.recv(1024)
+        # print(server_pub_key)
+        print('Client\'s public key received')
 
 
 def main():
-    a_client = AuthClient()
-    a_client.connect('', 4444)
+    try:
+        port = int(sys.argv[1])
+        bits_length = int(sys.argv[2])
+        a_client = AuthClient('', port, bits_length)
+        a_client.connect()
+        a_client.authenticate('user', 'password')
+    except ValueError:
+        print('Usage: python3 client.py <port> <key bits length>')
 
 
 if __name__=='__main__':
--- 3 year/comp. network security/client-server-auth/server.py
+++ 3 year/comp. network security/client-server-auth/server.py
@@ -1,4 +1,6 @@
 from socket import *
+from crypto import *
+import sys
 
 
 class AuthServer:
@@ -6,7 +8,7 @@ class AuthServer:
     Provides server functionality
     Like interface to work with socket and auth module
     """
-    def start(self, address: str, port: int) -> None:
+    def start(self, address: str, port: int, bits_length: int) -> None:
         """Start the server"""
         # Creata a socket object
         self.__socket = socket(AF_INET, SOCK_STREAM)
@@ -17,12 +19,25 @@ class AuthServer:
         self.__socket.listen()
         # Accept a new connection
         conn, addr = self.__socket.accept()
-        print(f'Connection from {addr[0]}:{addr[1]}')
+        print(f'Connection from {addr[0]}:{addr[1]}.')
+        client_pub_key = conn.recv(1024)
+        #print(client_pub_key)
+        print('Client\'s public key received')
+        rsa, pub_key, priv_key = generate_keys(bits_length)
+        self.__rsa = rsa
+        self.__pub_key = pub_key
+        self.__priv_key = priv_key
+        conn.send(str(pub_key).encode('utf-8'))
 
 
 def main():
-    a_server = AuthServer()
-    a_server.start('', 4444)
+    try:
+        a_server = AuthServer()
+        port = int(sys.argv[1])
+        bits_length = int(sys.argv[2])
+        a_server.start('', port, bits_length)
+    except ValueError:
+        print('Usage: python3 server.py <port> <key bits length>')
 
 
 if __name__=='__main__':
