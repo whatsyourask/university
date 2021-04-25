7d4c96c3d0dcf6e84f11494a02276127a930549f
whatsyourask <lexeymartynov@gmail.com>
2021-04-25 14:40:18 +0300

    I decided to move the functions of crypto in auth

--- 3 year/comp. network security/client-server-auth/auth.py
+++ 3 year/comp. network security/client-server-auth/auth.py
@@ -1,13 +1,38 @@
-from crypto import generate_keys
+import sys
+# To import FastModularExponentiation
+sys.path.insert(0, '../../computer security/')
+from rsa.backend import RSA, miller_rabin_generate
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 from re import fullmatch
 from typing import Tuple
 
 
 class Auth:
     """Class to share common functionality"""
-    def _generate_keys(self, bits_length: int) -> None:
+    def _generate_keys(self, bits_length: int) -> Tuple:
+        p = miller_rabin_generate(bits_length)
+        q = miller_rabin_generate(bits_length)
+        n = p * q
+        euler_func = (p - 1) * (q - 1)
+        rsa = RSA()
+        rsa.n = n
+        rsa.euler_func = euler_func
+        pub_key = (rsa.generate_public_key(bits_length), n)
+        priv_key = (rsa.generate_private_key(), n)
+        return rsa, pub_key, priv_key
+
+    def get_hash(self, passwd: str) -> str:
+        """Get a hash of the password"""
+        # TODO: Fix the issue with the hash object
+        digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+        digest.update(passwd.encode('utf-8'))
+        hash = digest.finalize()
+        return hash
+
+    def generate_keys(self, bits_length: int) -> None:
         """Generate keys and save them"""
-        rsa, pub_key, priv_key = generate_keys(bits_length)
+        rsa, pub_key, priv_key = self._generate_keys(bits_length)
         self._rsa = rsa
         self._pub_key = pub_key
         self._priv_key = priv_key
@@ -48,7 +73,7 @@ class Auth:
         """
         self._rsa.e = self._priv_key[0]
         encrypted_once = self._encrypt(data).split()
-        print(encrypted_once)
+        #print(encrypted_once)
         encrypted_twice = self._encrypt(encrypted_once, key)
-        print(encrypted_twice)
+        #print(encrypted_twice)
         return encrypted_twice
