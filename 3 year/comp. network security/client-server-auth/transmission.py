9b7569b77ce26048651495ad892a6e05d86be89f
whatsyourask <lexeymartynov@gmail.com>
2021-04-29 18:04:38 +0300

    Deleted the commented lines

--- 3 year/comp. network security/client-server-auth/transmission.py
+++ 3 year/comp. network security/client-server-auth/transmission.py
@@ -5,29 +5,18 @@ class Transmission:
         buff = []
         received = 0
         while received == self.BUF_SIZE or received == 0:
-            #print('RECEIVING')
             temp = conn.recv(self.BUF_SIZE)
             received = len(temp)
             if not received:
                 break
-            #print(received)
             buff.append(temp)
-            #print(temp)
         buff = b''.join(buff)
-        #print(buff.decode(self.ENCODING))
         return buff.decode(self.ENCODING)
 
     def sendall(self, conn, data: str) -> None:
         sent_length = 0
         data_length = len(data)
         data = data.encode(self.ENCODING)
-        #print(len(data))
         while sent_length < data_length:
-            #print('SENDING')
             sent = conn.send(data[sent_length:])
-            #print('sent', sent)
             sent_length += sent
-            #print('sent_length', sent_length)
-            # if not sent:
-            #     raise RuntimeError('Connection broken.')
-        #print('sending completed')
