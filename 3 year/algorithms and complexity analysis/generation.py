fc0bed8377d99efb678d97b8acedd0160c7d714b
whatsyourask <lexeymartynov@gmail.com>
2021-05-13 17:32:12 +0300

    Added generation of half sorted half random data and generation of reversed sorted data

--- 3 year/algorithms and complexity analysis/generation.py
+++ 3 year/algorithms and complexity analysis/generation.py
@@ -36,6 +36,51 @@ def generate_date(start, end):
     return start + timedelta(seconds=random_second)
 
 
+def generate_half_sorted(generate_func: callable, length: int, place: float) -> list:
+    array = generate_func(length)
+    part = int(length * place)
+    first_part = array[:part]
+    second_part = array[part:]
+    return sorted(first_part) + second_part
+
+
+def generate_half_sorted_numbers(length: int, percent: float) -> list:
+    return generate_half_sorted(generate_numbers, length, percent)
+
+
+def generate_half_sorted_integers(length: int, percent: float) -> list:
+    return generate_half_sorted(generate_integers, length, percent)
+
+
+def generate_half_sorted_strings(length: int, percent: float) -> list:
+    return generate_half_sorted(generate_strings, length, percent)
+
+
+def generate_half_sorted_dates(length: int, percent: float) -> list:
+    return generate_half_sorted(generate_dates, length, percent)
+
+
+def generate_reversed_sorted(generate_func: callable, length: int) -> list:
+    array = generate_func(length)
+    array.sort()
+    return array
+
+def generate_reversed_sorted_numbers(length: int) -> list:
+    return generate_reversed_sorted(generate_numbers, length)
+
+
+def generate_reversed_sorted_integers(length: int) -> list:
+    return generate_reversed_sorted(generate_integers, length)
+
+
+def generate_reversed_sorted_strings(length: int) -> list:
+    return generate_reversed_sorted(generate_strings, length)
+
+
+def generate_reversed_sorted_dates(length: int) -> list:
+    return generate_reversed_sorted(generate_dates, length)
+
+
 def main():
     numbers = generate_numbers(50)
     print(numbers)
@@ -47,6 +92,15 @@ def main():
     print(strings)
     dates = generate_dates(100)
     print(dates)
+    percent = 0.8
+    half_sorted_numbers = generate_half_sorted_numbers(50, percent)
+    print(half_sorted_numbers)
+    half_sorted_integers = generate_half_sorted_integers(50, percent)
+    print(half_sorted_integers)
+    half_sorted_strings = generate_half_sorted_strings(50, percent)
+    print(half_sorted_strings)
+    half_sorted_dates = generate_half_sorted_dates(50, percent)
+    print(half_sorted_dates)
 
 
 if __name__=='__main__':
