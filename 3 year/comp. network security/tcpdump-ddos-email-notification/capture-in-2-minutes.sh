c4346430a2d5e1d62e6a9e555b1f608b712270cf
root <root@home-workstation.home-workstation>
2021-05-23 19:46:35 +0300

    Fixed path to captured.pcap and captured.txt, also made full path to tcpdump

--- 3 year/comp. network security/tcpdump-ddos-email-notification/capture-in-2-minutes.sh
+++ 3 year/comp. network security/tcpdump-ddos-email-notification/capture-in-2-minutes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-tcpdump -i any -vn dst port 80 -G 120 -W 1 -w ~/captured.pcap
-tcpdump -nr captured.pcap | awk '{print $3,$6}' > captured.txt
+/usr/sbin/tcpdump -i any -vn dst port 80 -G 120 -W 1 -w ~/captured.pcap
+/usr/sbin/tcpdump -nr ~/captured.pcap | awk '{print $3,$6}' > ~/captured.txt
 # 0 * * * * ~/capture-in-2-minutes.sh
